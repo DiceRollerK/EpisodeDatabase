d723601775a0d9c4f117e73e102d5f2353c05198
Krapauskis <Arturs.Krapauskis@sk.lvt.lv>
2025-11-26 15:21:59 +0200

    NodeJS un SQLite sadarbošanās un mēģinājumi izvest datus no kādas tabulas uzspiežot pogu uz saites

--- database/nodejs-sqlite/index.js
+++ database/nodejs-sqlite/index.js
@@ -1,4 +1,13 @@
-import Database from 'better-sqlite3';
-const db = new Database('EpisodeDatabase.db');
+//import Database from 'better-sqlite3';
+//const db = new Database('EpisodeDatabase.db');
 
-console.log("Done");
+
+//Ievietot datus
+//const insert = db.prepare('INSERT INTO time (time_id, start_date, end_date) VALUES (?, ?, ?)');
+//insert.run('2','1989-07-05 12:00:00.0000','1998-05-14 12:00:00.0000');
+
+//Izvadīt datus
+//console.log(db.prepare('SELECT * FROM time;').all());
+
+
+console.log("Imported");
---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('test')
+//import Database from "./database/nodejs-sqlite/index.mjs";
+//const db = new Database('EpisodeDatabase.db');
+/*
+document.querySelector("#btn").addEventListener("click", () =>{
+    document.querySelector("#test-output").textContent = db.prepare('SELECT * FROM time;').all();
+})
+*/
